905895fdf8519e97ac94e66738472087ef9694d6
gjlim <erlking87@gmail.com>
2017-04-05 19:47:04 +0900

    ff

--- FaxtimeRest/server.js
+++ FaxtimeRest/server.js
@@ -1,6 +1,39 @@
-﻿var http = require('http');
+﻿'use strict';
+
+var port = process.env.PORT || 8000; // first change
+//var port = 8000;
+var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var swaggerize = require('swaggerize-express');
+var swaggerUi = require('swaggerize-ui'); // second change
+var path = require('path');
+
+var app = express();
+
+var server = http.createServer(app);
+
+app.use(bodyParser.json());
+
+app.use(swaggerize({
+    api: path.resolve('./config/swagger.json'), // third change
+    handlers: path.resolve('./handlers'),
+    docspath: '/swagger' // fourth change
+}));
+
+// change four
+app.use('/docs', swaggerUi({
+    docs: '/swagger'
+}));
+
+server.listen(port, function () { // fifth and final change
+});
+
+/*
+var http = require('http');
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
 }).listen(port);
+*/

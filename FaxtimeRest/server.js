d4a5e27972a4a661d2fee029915ee96e61201218
songhj <songhj@SONGHJ-PC>
2017-08-10 17:48:52 +0900

    timeout 수정

--- FaxtimeRest/server.js
+++ FaxtimeRest/server.js
@@ -19,8 +19,11 @@ var app = express();
 
 var server = http.createServer(app);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/v1/others/file/upload', upload.single('file'), (req, res, next) => {
     repository.uploadFile(req, res, next);
